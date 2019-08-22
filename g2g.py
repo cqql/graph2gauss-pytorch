f8293f011a2f0ab723901b666256a9e059766e50
Marten Lienen <marten.lienen@gmail.com>
2019-08-22 16:55:36 +0200

    Refactor with pytorch-ignite

--- g2g.py
+++ g2g.py
@@ -1,3 +1,4 @@
+import argparse
 import random
 
 import networkx
@@ -7,6 +8,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from ignite.engine import Engine, Events
+from ignite.handlers import ModelCheckpoint
+from torch.utils.data import DataLoader
+
+CHECKPOINT_PREFIX = "g2g"
 
 
 def load_dataset(filename):
@@ -14,14 +20,104 @@ def load_dataset(filename):
         return sp.csr_matrix(
             (file[f"{name}_data"], file[f"{name}_indices"], file[f"{name}_indptr"]),
             shape=file[f"{name}_shape"],
+            dtype=np.float32,
         )
 
     with np.load(filename) as f:
         A = load_sparse_matrix(f, "adj")
         X = load_sparse_matrix(f, "attr")
-        z = f["labels"]
+        z = f["labels"].astype(np.float32)
 
         return A, X, z
+
+
+class AttributedGraph:
+    def __init__(self, A, X, z):
+        self.A = A
+        self.X = X
+        self.z = z
+        self.level_sets = level_sets(A)
+
+        # Precompute the cardinality of each level set for every node
+        self.level_counts = {
+            node: np.array(list(map(len, level_sets)))
+            for node, level_sets in self.level_sets.items()
+        }
+
+        # Precompute the weights of each node's expected value in the loss
+        N = self.level_counts
+        self.loss_weights = 0.5 * np.array(
+            [N[i][1:].sum() ** 2 - (N[i][1:] ** 2).sum() for i in self.nodes()]
+        )
+
+    def nodes(self):
+        return range(self.A.shape[0])
+
+    def eligible_nodes(self):
+        """Nodes that can be used to compute the loss"""
+        N = self.level_counts
+
+        # If a node only has first-degree neighbors, the loss is undefined
+        return [i for i in self.nodes() if len(N[i]) >= 3]
+
+    def sample_two_neighbors(self, node, size=1):
+        level_sets = self.level_sets[node]
+        if len(level_sets) < 2:
+            raise Exception("1-partite graphs contain no edges")
+
+        level_counts = self.level_counts[node]
+        n = level_counts.sum()
+
+        node_map = [node for level_set in level_sets for node in level_set]
+
+        # Store the cardinality of the level set of each node
+        n_i = np.array(
+            [
+                count
+                for level_set, count in zip(level_sets, level_counts)
+                for node in level_set
+            ]
+        )
+
+        # Precompute the start of each level set in the node index set 1..n
+        start_i = np.array(
+            [
+                end - count
+                for level_set, count, end in zip(
+                    level_sets, level_counts, level_counts.cumsum()
+                )
+                for node in level_set
+            ]
+        )
+
+        # Each node has edges to every other node except the ones in its own level
+        # set
+        out_degrees = np.full(n, n) - n_i
+
+        # Sample nodes proportionally to their out-degree
+        p = out_degrees / out_degrees.sum()
+        source = np.random.choice(n, size=size, p=p, replace=True)
+
+        # For every source node, select one outgoing edge uniformly at random
+        dest = np.empty_like(source)
+        for i in range(size):
+            s = source[i]
+
+            # Sample from 1..(start_i[s]..+n_i[s]-1)..n
+            d = np.random.randint(out_degrees[s])
+            if d >= start_i[s]:
+                d += n_i[s]
+
+            dest[i] = d
+
+        edges = np.stack([source, dest], axis=1)
+
+        # Translate node indices back into entries from level_sets
+        for i in range(size):
+            edges[i][0] = node_map[edges[i][0]]
+            edges[i][1] = node_map[edges[i][1]]
+
+        return edges
 
 
 class Encoder(nn.Module):
@@ -37,6 +133,9 @@ class Encoder(nn.Module):
 
         """
         super().__init__()
+
+        self.D = D
+        self.L = L
 
         def xavier_init(layer):
             nn.init.xavier_normal_(layer.weight)
@@ -58,8 +157,64 @@ class Encoder(nn.Module):
 
         return mu, sigma
 
+    def compute_loss(self, graph, nsamples):
+        """Compute the energy-based loss from the paper
+        """
+
+        X = graph.X
+        weights = graph.loss_weights
+
+        loss = torch.tensor(0.0)
+        for i in graph.eligible_nodes():
+            # MC-estimate the loss function of this nodes' neighborhood graph
+            E = torch.tensor(0.0)
+            edges = graph.sample_two_neighbors(i, size=nsamples)
+            for edge in edges:
+                j, k = edge[0], edge[1]
+
+                mu, sigma = self.forward(torch.tensor(X[[i, j, k]].toarray()))
+                mu_i, sigma_i = mu[0], sigma[0]
+                mu_j, sigma_j = mu[1], sigma[1]
+                mu_k, sigma_k = mu[2], sigma[2]
+
+                diff_ij = mu_i - mu_j
+                closer = (
+                    0.5 * (sigma_j / sigma_i).sum()
+                    + (diff_ij / sigma_i).dot(diff_ij)
+                    - self.L
+                    - torch.log((sigma_j / sigma_i)).sum()
+                )
+                diff_ik = mu_i - mu_k
+                apart = (
+                    -0.5 * (sigma_k / sigma_i).sum()
+                    + (diff_ik / sigma_i).dot(diff_ik)
+                    - self.L
+                )
+
+                E += closer ** 2 + torch.exp(apart) * (
+                    (sigma_k / sigma_i).prod() + np.exp(0.5)
+                )
+            E /= nsamples
+
+            # Accumulate the weighted sum of expectations
+            loss += weights[i] * E
+
+        return loss
+
 
 def level_sets(A):
+    """Enumerate the level sets for each node's neighborhood
+
+    Parameters
+    ----------
+    A : np.array
+        Adjacency matrix
+
+    Returns
+    -------
+    { node: [i -> i-hop neighborhood] }
+    """
+
     G = networkx.from_scipy_sparse_matrix(A)
     paths = networkx.all_pairs_shortest_path(G)
 
@@ -75,34 +230,6 @@ def level_sets(A):
     return {root: reduce_paths(paths) for root, paths in paths}
 
 
-def level_counts(A):
-    """Count the nodes in each level set for each node
-
-    Parameters
-    ----------
-    A : np.array
-        Adjacency matrix
-
-    Returns
-    -------
-    { node: [i -> count of level set i] }
-    """
-
-    G = networkx.from_scipy_sparse_matrix(A)
-    paths = networkx.all_pairs_shortest_path(G)
-
-    def reduce_paths(paths):
-        max_depth = max(len(path) for path in paths.values())
-        counts = np.zeros(max_depth, dtype=np.int32)
-
-        for path in paths.values():
-            counts[len(path) - 1] += 1
-
-        return counts
-
-    return {root: reduce_paths(paths) for root, paths in paths}
-
-
 def train_test_split(n, train_ratio=0.5):
     nodes = list(range(n))
     split_index = int(n * train_ratio)
@@ -111,133 +238,79 @@ def train_test_split(n, train_ratio=0.5):
     return nodes[:split_index], nodes[split_index:]
 
 
-def sample_edges(level_sets, level_counts, size=1):
-    if len(level_sets) < 2:
-        raise Exception("1-partite graphs contain no edges")
-
-    n = level_counts.sum()
-
-    node_map = [node for level_set in level_sets for node in level_set]
-
-    # Store the cardinality of the level set of each node
-    n_i = np.array(
-        [
-            count
-            for level_set, count in zip(level_sets, level_counts)
-            for node in level_set
-        ]
-    )
-
-    # Precompute the start of each level set in the node index set 1..n
-    start_i = np.array(
-        [
-            end - count
-            for level_set, count, end in zip(
-                level_sets, level_counts, level_counts.cumsum()
-            )
-            for node in level_set
-        ]
-    )
-
-    # Each node has edges to every other node except the ones in its own level
-    # set
-    out_degrees = np.full(n, n) - n_i
-
-    # Sample nodes proportionally to their out-degree
-    p = out_degrees / out_degrees.sum()
-    source = np.random.choice(n, size=size, p=p, replace=True)
-
-    # For every source node, select one outgoing edge uniformly at random
-    dest = np.empty_like(source)
-    for i in range(size):
-        s = source[i]
-
-        # Sample from 1..(start_i[s]..+n_i[s]-1)..n
-        d = np.random.randint(out_degrees[s])
-        if d >= start_i[s]:
-            d += n_i[s]
-
-        dest[i] = d
-
-    edges = np.stack([source, dest], axis=1)
-
-    # Translate node indices back into entries from level_sets
-    for i in range(size):
-        edges[i][0] = node_map[edges[i][0]]
-        edges[i][1] = node_map[edges[i][1]]
-
-    return edges
-
-
 def main():
-    np.random.seed(0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--samples", type=int, default=3)
+    parser.add_argument("--seed", type=int)
+    parser.add_argument("-c", "--checkpoint")
+    parser.add_argument("--checkpoints")
+    parser.add_argument("dataset")
+    args = parser.parse_args()
 
-    A, X, z = load_dataset("citeseer.npz")
+    epochs = args.epochs
+    nsamples = args.samples
+    seed = args.seed
+    checkpoint_path = args.checkpoint
+    checkpoints_path = args.checkpoints
+    dataset_path = args.dataset
+
+    if seed:
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+
+    A, X, z = load_dataset(dataset_path)
+
     n = A.shape[0]
+    train_nodes, val_nodes = train_test_split(n)
+    A_train = A[train_nodes, :][:, train_nodes]
+    X_train = X[train_nodes]
+    z_train = z[train_nodes]
+    A_val = A[val_nodes, :][:, val_nodes]
+    X_val = X[val_nodes]
+    z_val = z[val_nodes]
 
-    train_nodes, test_nodes = train_test_split(n)
+    train_data = AttributedGraph(A_train, X_train, z_train)
+    val_data = AttributedGraph(A_val, X_val, z_val)
 
-    A_train = A[train_nodes, :][:, train_nodes].astype(np.float32)
-    X_train = X[train_nodes].astype(np.float32)
-    A_test = A[test_nodes, :][:, test_nodes]
-    X_test = X[test_nodes]
+    L = 10
+    encoder = Encoder(X.shape[1], L)
+    if checkpoint_path:
+        encoder.load_state_dict(torch.load(checkpoint_path))
 
-    L = level_sets(A_train)
-    N = {node: np.array(list(map(len, ls))) for node, ls in L.items()}
-    # Precompute the multiplier for each node in the loss function
-    F = np.array([N[i][1:].sum() ** 2 - (N[i][1:] ** 2).sum() for i in range(len(N))])
-
-    L_dim = 10
-    encoder = Encoder(X.shape[1], L_dim)
     optimizer = optim.Adam(encoder.parameters())
 
-    nsamples = 3
-    epochs = 5
-    for epoch in range(epochs):
+    def step(engine, dataset):
         optimizer.zero_grad()
 
-        loss = torch.tensor(0.0)
-        for i in range(A_train.shape[0]):
-            # If a node only has first-degree neighbors, the loss is undefined
-            if len(N[i]) < 3:
-                continue
-
-            E = torch.tensor(0.0)
-            edges = sample_edges(L[i][1:], N[i][1:], size=nsamples)
-            for edge in edges:
-                j, k = edge[0], edge[1]
-
-                mu, sigma = encoder(torch.tensor(X_train[[i, j, k]].toarray()))
-                mu_i, sigma_i = mu[0], sigma[0]
-                mu_j, sigma_j = mu[1], sigma[1]
-                mu_k, sigma_k = mu[2], sigma[2]
-
-                diff_ij = mu_i - mu_j
-                closer = (
-                    0.5 * (sigma_j / sigma_i).sum()
-                    + (diff_ij / sigma_i).dot(diff_ij)
-                    - L_dim
-                    - torch.log((sigma_j / sigma_i)).sum()
-                )
-                diff_ik = mu_i - mu_k
-                apart = (
-                    -0.5 * (sigma_k / sigma_i).sum()
-                    + (diff_ik / sigma_i).dot(diff_ik)
-                    - L_dim
-                )
-
-                E += closer ** 2 + torch.exp(apart) * (
-                    (sigma_k / sigma_i).prod() + np.exp(0.5)
-                )
-            E /= nsamples
-            loss += F[i] * E
-        loss *= 0.5
-
+        loss = encoder.compute_loss(dataset, nsamples)
         loss.backward()
 
-        print(f"Epoch {epoch:2d} - Loss {loss:.3f}")
-
         optimizer.step()
+
+        return loss.item()
+
+    trainer = Engine(step)
+
+    if checkpoints_path:
+        handler = ModelCheckpoint(
+            checkpoints_path, CHECKPOINT_PREFIX, n_saved=3, save_interval=1
+        )
+        trainer.add_event_handler(Events.EPOCH_COMPLETED, handler, {"encoder": encoder})
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_loss(engine):
+        print(f"Epoch {engine.state.epoch:2d} - Loss {engine.state.output:.3f}")
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def run_validation(engine):
+        if engine.state.epoch % 5 != 1:
+            return
+
+        loss = encoder.compute_loss(val_data, nsamples)
+        print(f"Validation loss {loss:.3f}")
+
+    trainer.run([train_data], epochs)
 
 
 if __name__ == "__main__":
